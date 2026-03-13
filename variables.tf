6242054fea97f445cd58aa79213d3fdfe834bd03
shubham <shubhamtippe9@gmail.com>
2026-03-13 16:12:21 +0530

    1st commit

---
+++ variables.tf
@@ -0,0 +1,19 @@
+variable "cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "sub1_cidr" {
+  default = "121.12.1.10/18"
+}
+
+variable "sub2_cidr" {
+  default = "121.12.1.0/20"
+}
+
+variable "ami_id" {
+  default = "ami-02dfbd4ff395f2a1b"
+}
+
+variable "insatance_type" {
+  default = "t3.micro"
+}
